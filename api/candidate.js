822ec8004efc0e5faf3118d8df733772cb4f5b18
asitkp <asit.asu96@gmail.com>
2024-01-28 16:40:08 +0530

    added delete method

--- api/candidate.js
+++ api/candidate.js
@@ -121,3 +121,28 @@ module.exports.get = (event, context, callback) => {
       return;
     });
 };
+
+module.exports.delete = (event, context, callback) => {
+  const params = {
+    TableName: process.env.CANDIDATE_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  dynamoDb.delete(params).promise()
+    .then(result => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Sucessfully deleted the candidate`,
+        })
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      console.error(error);
+      callback(new Error('Couldn\'t Delete candidate.'));
+      return;
+    });
+};
